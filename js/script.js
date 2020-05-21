89a574537068ce44068e5ec7ecd59237d801a7c7
Andrea <andryx1994@gmail.com>
2020-05-21 10:59:36 +0200

    Versione 1.2
    Rivisto codice JS

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
 alert("Calcola il prezzo del viaggio inserendo la distanza e la tua età!");
 
+var chilometri;
 document.getElementById('chilometri').innerHTML =  chilometri = Math.abs(parseInt(prompt("Dimmi i chilometri del tuo viaggio")));
+var anni;
 document.getElementById('anni').innerHTML =  anni = Math.abs(parseInt(prompt("Dimmi quanti anni hai")));
+
 var prezzo = (chilometri * 0.21);
+var prezzoJunior = prezzo - (prezzo * 0.2); // applica lo sconto del 20% per i minorenni
+var prezzoSenior = prezzo - (prezzo * 0.4); // applica lo sconto del 40% per gli over 65
 
 if (isNaN(chilometri) || isNaN(anni)) {
   alert("Inserisci correttamente i dati!")
 }
+
 else {
   if (anni < 18) {
-    var prezzo = prezzo - (prezzo * 0.2); // applica lo sconto del 20% per i minorenni
-    document.getElementById('prezzo').innerHTML =  prezzo.toFixed(2);
+    document.getElementById('prezzo').innerHTML =  prezzoJunior.toFixed(2);
   }
 
   else if (anni > 65){
-    var prezzo = prezzo - (prezzo * 0.4); // applica lo sconto del 40% per i maggiorenni
-    document.getElementById('prezzo').innerHTML =  prezzo.toFixed(2);
+    document.getElementById('prezzo').innerHTML =  prezzoSenior.toFixed(2);
   }
 
   else {
