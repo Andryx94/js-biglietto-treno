e6223b956e085b92c481a6993f64092bb82cbe1e
Andrea <andryx1994@gmail.com>
2020-05-21 10:29:24 +0200

    Versione 1.1
    Sostituito anno di nascita con anni correnti

--- js/script.js
+++ js/script.js
@@ -1,22 +1,19 @@
-alert("Calcola il prezzo del viaggio inserendo la distanza e il tuo anno di nascita!");
+alert("Calcola il prezzo del viaggio inserendo la distanza e la tua età!");
 
-var data = new Date();
-var annoCorrente = data.getFullYear();
-document.getElementById('chilometri').innerHTML =  chilometri = parseInt(prompt("Dimmi i chilometri del tuo viaggio"));
-document.getElementById('annoNascita').innerHTML =  annoNascita = parseInt(prompt("Dimmi il tuo anno di nascita"));
+document.getElementById('chilometri').innerHTML =  chilometri = Math.abs(parseInt(prompt("Dimmi i chilometri del tuo viaggio")));
+document.getElementById('anni').innerHTML =  anni = Math.abs(parseInt(prompt("Dimmi quanti anni hai")));
 var prezzo = (chilometri * 0.21);
-var differenzaAnni = annoCorrente - annoNascita;
 
-if (isNaN(chilometri) || isNaN(annoNascita)) {
+if (isNaN(chilometri) || isNaN(anni)) {
   alert("Inserisci correttamente i dati!")
 }
 else {
-  if (differenzaAnni < 18) {
+  if (anni < 18) {
     var prezzo = prezzo - (prezzo * 0.2); // applica lo sconto del 20% per i minorenni
     document.getElementById('prezzo').innerHTML =  prezzo.toFixed(2);
   }
 
-  else if (differenzaAnni > 65){
+  else if (anni > 65){
     var prezzo = prezzo - (prezzo * 0.4); // applica lo sconto del 40% per i maggiorenni
     document.getElementById('prezzo').innerHTML =  prezzo.toFixed(2);
   }
